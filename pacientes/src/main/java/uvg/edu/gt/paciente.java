05808959a75ebc6b55b73b4c8116526d4eb8cac9
PablorellanaM <ore20555@uvg.edu.gt>
2024-04-09 22:14:29 -0600

    Creacion clase Paciente

---
+++ pacientes/src/main/java/uvg/edu/gt/paciente.java
@@ -0,0 +1,25 @@
+package uvg.edu.gt;
+
+/**
+ * Representa un paciente con nombre, síntoma y código de emergencia.
+ */
+public class Paciente implements Comparable<Paciente> {
+    private String nombre;
+    private String sintoma;
+    private char codigoEmergencia;
+
+    /**
+     * Crea un nuevo paciente con los detalles proporcionados.
+     * 
+     * @param nombre           Nombre del paciente.
+     * @param sintoma          Descripción del síntoma del paciente.
+     * @param codigoEmergencia Código de emergencia que indica la prioridad de
+     *                         atención.
+     */
+    public Paciente(String nombre, String sintoma, char codigoEmergencia) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigoEmergencia = codigoEmergencia;
+    }
+
+}
